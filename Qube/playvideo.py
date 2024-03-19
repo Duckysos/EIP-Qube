71f6becc720d56b31db8aa0c65f2c442aee99255
Duckysos <iankho2002@gmail.com>
2024-03-19 13:26:33 +0000

    update playvideo.py

--- Qube/playvideo.py
+++ Qube/playvideo.py
@@ -1,23 +1,53 @@
+import threading
 import subprocess
-import time
 
-# Function to play video using VLC
-def play_video(video_path):
-    # Using the cvlc command to play the video
-    # Adding '--play-and-exit' to make sure VLC closes after playing the video
-    subprocess.run(['cvlc', '--play-and-exit', video_path])
+class VideoPlayer(threading.Thread):
 
-# Example program state
-program_state = 'state1'
+    def __init__(self, videos):
+        threading.Thread.__init__(self)
+        self.videos = videos
+        self.current_state = None
+        self.next_state = None
+        self.running = True
+
+    def run(self):
+        while self.running:
+            if self.next_state != self.current_state:
+                self.current_state = self.next_state
+                video_path = self.videos.get(self.current_state)
+                if video_path:
+                    self.play_video(video_path)
+
+    def play_video(self, video_path):
+        # Use VLC to play video
+        subprocess.call(['cvlc', '--play-and-exit', video_path])
+
+    def change_state(self, new_state):
+        self.next_state = new_state
+
+    def stop(self):
+        self.running = False
 
 # Video files for different states
 videos = {
-    'state1': 'C:\\Users\\iankh\\Documents\\GitHub\\EIP-Qube\\videos\\Mousey Answer Correct.avi',
-
+    'state1': '/home/pi/EIP-Qube/videos/Mousey Answer Correct.avi',
+    'state2': '/home/pi/EIP-Qube/videos/Mousey Idle.avi',
+    'state3': '/home/pi/EIP-Qube/videos/Mousey Listening.avi',
+    'state4': '/home/pi/EIP-Qube/videos/Mousey Waiting(Loading).avi',
+    'state5': '/home/pi/EIP-Qube/videos/Mousey Wrong Answer.avi'
 }
 
-# Play the video based on the program state
-if program_state in videos:
-    play_video(videos[program_state])
-else:
-    print("No video for the current state.")
+if __name__ == '__main__':
+    # Initialize the video player with the state-video mapping
+    player = VideoPlayer(videos)
+    player.start()
+
+    # Example of changing states
+    player.change_state('state1')
+    # Wait or perform other tasks...
+    player.change_state('state2')
+    # Continue with your application logic...
+
+    # Stop the video player when done
+    player.stop()
+    player.join()
